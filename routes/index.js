e94cf9f4de636ebb80e78fe42e14bb0a35ffe729
vania-leite <ei12037@fe.up.pt>
2016-04-08 16:33:32 +0100

    initial setup desta theme, tou a ver como funciona so

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/dashboard', function(req, res, next) {
+  console.log('the response will be sent by the next function ...');
+  res.render('dashboard', { title: 'Stats' });
+});
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/dashboard', function(req, res, next) {
+  res.send('dashboard', { title: 'Stats' });
 });
 
 module.exports = router;
