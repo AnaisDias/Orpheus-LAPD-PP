daa2033fb806d479cc7334732878494dfee83f6d
vania-leite <ei12037@fe.up.pt>
2016-04-27 22:49:41 +0100

    update: creating user on login if not exists

--- controllers/fitbit-auth.js
+++ controllers/fitbit-auth.js
@@ -3,6 +3,8 @@ var request = require("request");
 var fitbit_accessToken, fitbit_refreshToken, fitbit_profile;
 var FitbitStrategy = require('passport-fitbit-oauth2').FitbitOAuth2Strategy;
 
+var models = require('../models');
+
 // Configure Passport session management to use the Fitbit user
 // These de/serialize functions work for this hack but are not
 // well suited for "real world" apps because you'd want to persist
@@ -28,6 +30,15 @@ function(accessToken, refreshToken, profile, done) {
   fitbit_refreshToken=refreshToken;
   fitbit_profile=profile;
   console.log(fitbit_profile);
+
+  models.User.findOrCreate({
+		where: { auth_id: fitbit_profile.id}, // we search for this user
+		defaults: {fullname: fitbit_profile._json.user.fullName, avatar: fitbit_profile._json.user.avatar, displayName: fitbit_profile._json.user.displayName, gender: fitbit_profile._json.user.gender, age: fitbit_profile._json.user.age}});
+
+
+
+	console.log("auth id: "+fitbit_profile.id);
+  console.log(JSON.stringify(fitbit_profile._json.user.displayName));
   done(null, {
     accessToken: accessToken,
     refreshToken: refreshToken,
