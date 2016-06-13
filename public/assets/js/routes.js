ba6fe1550ec94a5bbd47ea8b2e964c257b4b6c19
vania-leite <ei12037@fe.up.pt>
2016-06-13 02:38:57 +0100

    patientlist template url therapist?id=x

--- public/assets/js/routes.js
+++ public/assets/js/routes.js
@@ -82,6 +82,47 @@ angular
                 }
             })
 
+            .state('app.therapist', {
+                url: '/therapist?id',
+                templateUrl: 'views/patientlist.html',
+                //page title goes here
+                ncyBreadcrumb: {
+                    label: '{{ "TITLE" | translate }}'
+                },
+                //page subtitle goes here
+                params: {
+                    subtitle: 'Welcome to e-Health'
+                },
+                resolve: {
+                    loadPlugin: ['$ocLazyLoad', function($ocLazyLoad) {
+                        // you can lazy load files for an existing module
+                        return $ocLazyLoad.load([{
+                            serie: true,
+                            files: ['assets/js/libs/moment.min.js']
+                        }, {
+                            serie: true,
+                            files: ['assets/js/libs/daterangepicker.min.js', 'assets/js/libs/angular-daterangepicker.min.js']
+                        }, {
+                            serie: true,
+                            name: 'chart.js',
+                            files: ['assets/js/libs/Chart.min.js', 'assets/js/libs/angular-chart.min.js']
+                        }, {
+                            serie: true,
+                            files: ['assets/js/libs/gauge.min.js']
+                        }, {
+                            serie: true,
+                            files: ['assets/js/libs/angular-toastr.tpls.min.js']
+                        }]);
+                    }],
+                    loadMyCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
+                        // you can lazy load controllers
+                        return $ocLazyLoad.load({
+                            files: ['assets/js/controllers/main.js']
+                        });
+                    }]
+                }
+            })
+
 
 
         .state('appSimple', {
