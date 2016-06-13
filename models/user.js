220652f8e47073fd3c8629c1d434ac38d6004490
Joao-Ramos <joaoramos-13@hotmail.com>
2016-06-13 06:41:09 +0100

    changed user module because of login

--- models/user.js
+++ models/user.js
@@ -2,11 +2,18 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     fullname: DataTypes.STRING,
+    username: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    type: DataTypes.INTEGER,
+    therapist: DataTypes.INTEGER,
     avatar: DataTypes.STRING,
     displayName: DataTypes.STRING,
     gender: DataTypes.STRING,
     age: DataTypes.INTEGER,
-    auth_id: DataTypes.STRING
+    auth_id: DataTypes.STRING,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
   }, {
     classMethods: {
       associate: function(models) {
